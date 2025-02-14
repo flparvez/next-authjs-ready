bf77138eaaff4d1c715255afdfcebcf6d584120f
flparvez <flparvez23@gmail.com>
2025-02-14 11:40:02 +0600

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Head from '@/components/Header'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <Head />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+"use client"
+import { useSession,signOut } from 'next-auth/react';
+import React from 'react'
+import { useNotification } from "@/components/Notification";
+import Link from 'next/link';
+const Head = () => {
+  const { data: session } = useSession();
+  const { showNotification } = useNotification();
+  const handleSignOut = async () => {
+    try {
+      await signOut();
+      showNotification("Signed out successfully", "success");
+      
+    } catch {
+      showNotification("Failed to sign out", "error");
+    }
+  };
+  console.log(session)
+  return (
+    <div>
+      <h2>This is home page</h2>
+      <h2><Link href="/auth/login">Login</Link></h2>
+      <h2><Link href="/auth/register">Register</Link></h2>
+      <h1>Name : {session?.user?.name}</h1>
+      <h1>Email : {session?.user?.email}</h1>
+      <br />
+      <button onClick={handleSignOut}>Sign Out</button>
+    </div>
+  )
+}
+
+export default Head
